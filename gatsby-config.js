318b8bd488f44f3208ffb650590ea25a09b763f6
William Roberts <will@moonandcrater.com>
2019-06-26 20:33:44 -0600

    added non purged css file

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,6 @@ module.exports = {
       options: {
         tailwind: true,
         purgeOnly: ['/src/css/styles.css'],
-        printRejected: true,
       },
     },
     'gatsby-plugin-netlify',
